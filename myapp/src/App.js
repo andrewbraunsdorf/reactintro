73998f36e7c6efab3d80c106f738bf4211aadd3d
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-04 23:44:20 +0000

    added const { ninjas } = this.props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,14 +3,20 @@ import Ninjas from './ninjas';
 
 
 class App extends Component {
+  state = {
+    ninjas : [
+    { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+    { name: 'Yoshi', age: 25, belt: 'green', id: 2 }, 
+    { name: 'Crystal', age: 20, belt: 'red', id: 3 }
+        ]
+  }
   render() {
     
     return (
       <div className="App">-
           <h1>My first react app!</h1>
           <p>Welcome</p>
-          <Ninjas name="Ryu" age='25' belt="black"/>
-          <Ninjas name="Yoshi" age='30' belt="red"/>
+          <Ninjas ninjas={this.state.ninjas}/>
       </div>
     );
   }
