66cc43af4336d9d0b5f10fe56ff784d63f4d4f1c
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-04 22:56:41 +0000

    added props and store properties

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,11 +4,13 @@ import Ninjas from './ninjas';
 
 class App extends Component {
   render() {
+    
     return (
-      <div className="App">
+      <div className="App">-
           <h1>My first react app!</h1>
           <p>Welcome</p>
-          <Ninjas/>
+          <Ninjas name="Ryu" age='25' belt="black"/>
+          <Ninjas name="Yoshi" age='30' belt="red"/>
       </div>
     );
   }
