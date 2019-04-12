dea75325eedd28a3b2541b391da346ab423fd3d1
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-12 00:08:16 +0000

    output conditionally using if statement or ternary operator

--- myapp/src/ninjas.js
+++ myapp/src/ninjas.js
@@ -1,23 +1,37 @@
 import React from 'react';
 
-const Ninjas = ({ninjas}) => {
+const Ninjas = ({ ninjas }) => {
 	// no longer needed because you are destructuring inside the the above function
 	// const { ninjas } = props;
-	const ninjaList = ninjas.map(ninja => {
-		return (
-			<div className="ninja" key={ninja.id}>
-			<div> Name: { ninja.name } </div>
-			<div> Age: { ninja.age } </div>
-			<div> Belt: { ninja.belt } </div>
-			</div>
-		);
-	});
+	// const ninjaList = ninjas.map(ninja => {
+	// 	if (ninja.age > 20) {
+	// 		return (
+	// 			<div className="ninja" key={ninja.id}>
+	// 		<div> Name: { ninja.name } </div>
+	// 		<div> Age: { ninja.age } </div>
+	// 		<div> Belt: { ninja.belt } </div>
+	// 		</div>
+	// 		);
+	// 	} else {
+	// 		return null;
+	// 	}
+	// });
+
+
 	return (
 		<div className="ninja-list">
-				{ ninjaList }
+				{ 	
+				ninjas.map(ninja => {
+		return ninja.age > 20 ? (
+			<div className="ninja" key={ninja.id}>
+				<div> Name: { ninja.name } </div>
+				<div> Age: { ninja.age } </div>
+				<div> Belt: { ninja.belt } </div>
+			</div>) : null;
+	})
+	}
 			</div>
 	);
+};
 
-}
-
-export default Ninjas
+export default Ninjas;
