b0eded219209a79da5088a1e31ee93a5e56fc915
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-15 23:49:34 +0000

    added basics of form for updating name, age, and belt on the fly

---
+++ myapp/src/AddNinja.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class AddNinja extends Component {
+	state = {
+		name: null,
+		age: null,
+		belt: null
+	}
+	handleChange = (e) => {
+		this.setState({
+			
+		})
+	}
+	render() {
+		return (
+			<div>
+				<form>
+					<label htmlFor='name'>Name:</label>
+					<input type="text" id="name" onChange = {} />
+					<label htmlFor='name'>Age:</label>
+					<input type="text" id="age" onChange = {} />
+					<label htmlFor='name'>Belt:</label>
+					<input type="text" id="belt" onChange = {} />
+					<button>Submit</button>
+				</form>
+			</div>
+			
+			)
+	}
+}
