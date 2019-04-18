38a82dc4d58b2c78f1a0cd86159a70e3e6b823f2
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-18 00:51:14 +0000

    added function to handle adding todos

--- myapp/src/AddTodo.js
+++ myapp/src/AddTodo.js
@@ -7,18 +7,21 @@ class AddTodo extends Component {
 	handleChange = (e) => {
 		this.setState({
 			content: e.target.value
-		})
+		});
 	}
 	handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(this.state);
+		this.props.addTodo(this.state);
+		this.setState({
+			content: ''
+		});
 	}
 	render() {
 		return (
 			<div>
 			<form onSubmit={this.handleSubmit}>
 				<label>Add new todo:</label>
-				<input type='text' onChange={this.handleChange}/>
+				<input type='text' onChange={this.handleChange} value={this.state.content}/>
 			</form>
 			</div>
 		);
