c5104b08ffa4a5a1de37c0ceccee0589399088ca
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-20 16:30:51 +0000

    added  Post component to get the :post_id

---
+++ myapp/src/components/Post.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Post extends	Component {
+	render() {
+		return (
+				<div className='container'>
+					<h4>Route Param</h4>
+				</div>
+			)
+	}
+}
+
+export default Post
