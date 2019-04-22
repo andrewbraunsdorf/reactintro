90be7d148ded579f7bdcc1661e6b2ed4727172ff
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-22 00:37:09 +0000

    added redux basics to store to output object

--- myapp/reduxIntro.js
+++ myapp/reduxIntro.js
@@ -6,7 +6,11 @@ const initState = {
 };
 
 function myreducer(state = initState, action){
-  
+  console.log(action, state)
 }
 
 const store = createStore(myreducer);
+
+const todoAction = { type: 'ADD_TODO', todo: 'buy milk'};
+
+store.dispatch(todoAction);
