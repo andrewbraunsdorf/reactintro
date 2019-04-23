207f371c1c0d5f87365a7e40e047106d9a0be9ea
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-23 00:33:49 +0000

    changed the state of todos

--- myapp/reduxIntro.js
+++ myapp/reduxIntro.js
@@ -6,7 +6,11 @@ const initState = {
 };
 
 function myreducer(state = initState, action){
-  console.log(action, state)
+  if (action.type == 'ADD_TODO'){
+  	return {
+  		todos: [...state.todos, action.todo]
+  	};
+  }
 }
 
 const store = createStore(myreducer);
