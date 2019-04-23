628fcc2fae0869c6bbaa1619f3a90048c802252a
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-23 01:00:18 +0000

    added todo and posts

--- myapp/reduxIntro.js
+++ myapp/reduxIntro.js
@@ -8,7 +8,15 @@ const initState = {
 function myreducer(state = initState, action){
   if (action.type == 'ADD_TODO'){
   	return {
+  		...state,
   		todos: [...state.todos, action.todo]
+  	};
+  }
+  
+    if (action.type == 'ADD_POST'){
+  	return {
+  		...state,
+  		posts: [...state.posts, action.post]
   	};
   }
 }
@@ -17,9 +25,13 @@ const store = createStore(myreducer);
 
 store.subscribe(() => {
 	console.log('state updated');
-	console.log(store.getStated());
+	console.log(store.getState());
 });
 
-const todoAction = { type: 'ADD_TODO', todo: 'buy milk'};
+// const todoAction = { type: 'ADD_TODO', todo: 'buy milk'};
 
-store.dispatch(todoAction);
+// store.dispatch(todoAction);
+
+store.dispatch({type: 'ADD_TODO', todo: 'buy milk'});
+store.dispatch({type: 'ADD_TODO', todo: 'sleep some more'});
+store.dispatch({type: 'ADD_POST', post: 'Egg hunt with Yoshi'});
